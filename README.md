8d0e5383c91b45b73870c3d86d49a91a9f2fc089
Yuraima Estevez <yurm04@gmail.com>
2025-11-13 19:02:17 -0600

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+## Hex Appeal
+
+For Raquel
+
+<3 Dom and Yurm
+
+```sh
+trunk serve --port 3000 --open
+```
---
+++ src/main.rs
@@ -0,0 +1,14 @@
+use hex_appeal::App;
+use leptos::prelude::*;
+
+fn main() {
+	// set up logging
+	_ = console_log::init_with_level(log::Level::Debug);
+	console_error_panic_hook::set_once();
+
+	mount_to_body(|| {
+		view! {
+				<App />
+		}
+	})
+}
