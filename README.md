4d175631e9d660ed01f7b68a1e2684cabcc15566
Dominik Wilkowski <Hi@Dominik-Wilkowski.com>
2025-11-14 12:45:11 +1000

    Added favicon, cleaned, added action for test and release

--- README.md
+++ README.md
@@ -4,6 +4,31 @@ For Raquel
 
 <3 Dom and Yurm
 
+## Requirements
+
+- Install rust nightly `rustup toolchain install nightly --allow-downgrade`
+- Install WASM target `rustup target add wasm32-unknown-unknown`
+- Install cargo-generate, trunk and leptosfmt `cargo install cargo-generate trunk leptosfmt --locked`
+
+## Development
+
+Dev locally:
+
 ```sh
 trunk serve --port 3000 --open
 ```
+
+Before committing make sure you run:
+```sh
+leptosfmt ./**/*.rs
+```
+
+## Release
+
+Locally you can run:
+
+```sh
+trunk build --release
+```
+
+For a real release we GitHub action which will be triggered by a release.
--- src/pages/home.rs
+++ src/pages/home.rs
@@ -5,48 +5,34 @@ use leptos::prelude::*;
 #[component]
 pub fn Home() -> impl IntoView {
 	view! {
-			<ErrorBoundary fallback=|errors| {
-					view! {
-							<h1>"Uh oh! Something went wrong!"</h1>
+		<ErrorBoundary fallback=|errors| {
+			view! {
+				<h1>"Uh oh! Something went wrong!"</h1>
 
-							<p>"Errors: "</p>
-							// Render a list of errors as strings - good for development purposes
-							<ul>
-									{move || {
-											errors
-													.get()
-													.into_iter()
-													.map(|(_, e)| view! { <li>{e.to_string()}</li> })
-													.collect_view()
-									}}
+				<p>"Errors: "</p>
+				// Render a list of errors as strings - good for development purposes
+				<ul>
+					{move || {
+						errors
+							.get()
+							.into_iter()
+							.map(|(_, e)| view! { <li>{e.to_string()}</li> })
+							.collect_view()
+					}}
 
-							</ul>
-					}
-			}>
+				</ul>
+			}
+		}>
 
-					<div class="container">
+			<main class="container">
+				<h1>"Welcome to hex appeal"</h1>
 
-							<picture>
-									<source
-											srcset="https://raw.githubusercontent.com/leptos-rs/leptos/main/docs/logos/Leptos_logo_pref_dark_RGB.svg"
-											media="(prefers-color-scheme: dark)"
-									/>
-									<img
-											src="https://raw.githubusercontent.com/leptos-rs/leptos/main/docs/logos/Leptos_logo_RGB.svg"
-											alt="Leptos Logo"
-											height="200"
-											width="400"
-									/>
-							</picture>
+				<div class="buttons">
+					<Button />
+					<Button increment=5 />
+				</div>
 
-							<h1>"Welcome to Leptos"</h1>
-
-							<div class="buttons">
-									<Button />
-									<Button increment=5 />
-							</div>
-
-					</div>
-			</ErrorBoundary>
+			</main>
+		</ErrorBoundary>
 	}
 }
