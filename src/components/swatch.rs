fd9f4c82c79ee66077aacee09b64716f49d3e6ec
Dominik Wilkowski <Hi@Dominik-Wilkowski.com>
2025-11-24 13:36:48 +1100

    fixed typo

--- src/components/swatch.rs
+++ src/components/swatch.rs
@@ -5,5 +5,5 @@ use crate::pages::home::Color;
 #[component]
 pub fn Swatch(color: Color, idx: ReadSignal<usize>) -> impl IntoView {
 	let style = format!("background:rgb({}, {}, {})", color.value.r, color.value.g, color.value.b,);
-	view! { <li style=style>{color.name.clone()} <button>Detele: {idx.get()}</button></li> }
+	view! { <li style=style>{color.name.clone()} <button>Delete: {idx.get()}</button></li> }
 }
