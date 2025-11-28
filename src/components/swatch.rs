356a3b7a4405e6ab427f0b9be70ff2a58b69546a
Dominik Wilkowski <Hi@Dominik-Wilkowski.com>
2025-11-29 07:47:32 +1000

    more swatch styles

--- src/components/swatch.rs
+++ src/components/swatch.rs
@@ -14,22 +14,26 @@ pub fn Swatch(
 ) -> impl IntoView {
 	let style = format!("background:rgb({}, {}, {})", color.value.red, color.value.green, color.value.blue,);
 	view! {
-		<li style=style class="swatch">
-			<div class="swatch_label">
-				{color.name.clone()}
-				<DelButton on_click=move |_| {
-					let g_idx = group_idx.get_untracked();
-					let c_idx = idx.get_untracked();
-					set_groups
-						.update(move |groups| {
-							if let Some(group) = groups.get_mut(g_idx) {
-								if c_idx < group.colors.len() {
-									group.colors.remove(c_idx);
+		<li class="swatch">
+			<div class="swatch_color" style=style />
+			<ul class="swatch_label">
+				<li class="swatch_label_name">
+					{color.name.clone()}
+					<DelButton on_click=move |_| {
+						let g_idx = group_idx.get_untracked();
+						let c_idx = idx.get_untracked();
+						set_groups
+							.update(move |groups| {
+								if let Some(group) = groups.get_mut(g_idx) {
+									if c_idx < group.colors.len() {
+										group.colors.remove(c_idx);
+									}
 								}
-							}
-						});
-				}>"Delete"</DelButton>
-			</div>
+							});
+					}>"Delete"</DelButton>
+				</li>
+				<li class="swatch_label_value">"RGB("{color.value.red}", "{color.value.green}", "{color.value.blue}")"</li>
+			</ul>
 		</li>
 	}
 }
