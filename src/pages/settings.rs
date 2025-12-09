d95ae77028579ef36bde77d30d261b8d4d3ed152
Dominik Wilkowski <Hi@Dominik-Wilkowski.com>
2025-12-10 06:30:44 +1000

    added reset button to settings

--- src/pages/settings.rs
+++ src/pages/settings.rs
@@ -1,8 +1,20 @@
 use leptos::prelude::*;
+use leptos_router::hooks::use_navigate;
 
-// use crate::{};
+use crate::{components::del_button::DelButton, Groups};
 
 #[component]
 pub fn Settings() -> impl IntoView {
-	view! { "test" }
+	let set_groups = use_context::<WriteSignal<Groups>>().expect("Unable to find set_groups context");
+	let navigate = use_navigate();
+
+	view! {
+		<DelButton on_click=move |_| {
+			set_groups
+				.update(move |groups| {
+					*groups = Groups::default();
+				});
+			navigate("/", Default::default());
+		}>"Reset all data"</DelButton>
+	}
 }
