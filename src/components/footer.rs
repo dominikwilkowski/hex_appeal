aec85f4e3d0d5cdddc04497ce3d0589901e45b28
Dominik Wilkowski <Hi@Dominik-Wilkowski.com>
2026-01-16 09:30:36 +1000

    added version to footer

--- src/components/footer.rs
+++ src/components/footer.rs
@@ -3,9 +3,12 @@ use leptos_router::components::A;
 
 #[component]
 pub fn Footer() -> impl IntoView {
+	let version = env!("CARGO_PKG_VERSION");
+
 	view! {
 		<footer>
 			<A href="/settings">Settings</A>
+			<span class="version">v{version}</span>
 		</footer>
 	}
 }
