ee7833688cc68fffeb160b076e233f280a6fddb9
Dominik Wilkowski <Hi@Dominik-Wilkowski.com>
2026-01-17 08:51:36 +1000

    added css for open button

--- src/components/new_group.rs
+++ src/components/new_group.rs
@@ -35,96 +35,98 @@ pub fn NewGroup(group_idx: ReadSignal<usize>) -> impl IntoView {
 	};
 
 	view! {
-		<Show
-			when=move || { show_form.get() }
-			fallback=move || {
-				view! { <button on:click=move |_| set_show_form.set(true)>"+"</button> }
-			}
-		>
-			<form on:submit=on_submit>
-				<button on:click=move |_| set_show_form.set(false)>"x"</button>
-				<ul>
-					<li class="new_swatch_name">
-						<label>
-							"Name: "
-							<input
-								type="text"
-								required
-								prop:value=name
-								on:input=move |ev| {
-									set_name.set(event_target_value(&ev));
-								}
-							/>
-						</label>
-					</li>
-					<li class="new_swatch_rgb">
-						<label>
-							"R: "
-							<input
-								type="number"
-								required
-								min="0"
-								max="255"
-								prop:value=r
-								on:input=move |ev| {
-									set_r.set(event_target_value(&ev));
-								}
-							/>
-						</label>
-					</li>
-					<li class="new_swatch_rgb">
-						<label>
-							"G: "
-							<input
-								type="number"
-								required
-								min="0"
-								max="255"
-								prop:value=g
-								on:input=move |ev| {
-									set_g.set(event_target_value(&ev));
-								}
-							/>
-						</label>
-					</li>
-					<li class="new_swatch_rgb">
-						<label>
-							"B: "
-							<input
-								type="number"
-								required
-								min="0"
-								max="255"
-								prop:value=b
-								on:input=move |ev| {
-									set_b.set(event_target_value(&ev));
-								}
-							/>
-						</label>
-					</li>
-					<li>
-						<label>
-							"Color: "
-							<input
-								type="color"
-								prop:value=color
-								on:input=move |ev| {
-									let value = event_target_value(&ev);
-									if let Some(rgb) = Rgb::from_hex(&value) {
-										set_r.set(rgb.red.to_string());
-										set_g.set(rgb.green.to_string());
-										set_b.set(rgb.blue.to_string());
+		<div class="new_group_form">
+			<Show
+				when=move || { show_form.get() }
+				fallback=move || {
+					view! { <button class="new_group_btn_open" on:click=move |_| set_show_form.set(true)>"+"</button> }
+				}
+			>
+				<form on:submit=on_submit>
+					<button class="new_group_btn_close" on:click=move |_| set_show_form.set(false)>"x"</button>
+					<ul>
+						<li class="new_swatch_name">
+							<label>
+								"Name: "
+								<input
+									type="text"
+									required
+									prop:value=name
+									on:input=move |ev| {
+										set_name.set(event_target_value(&ev));
 									}
-									set_color.set(value);
-								}
-							/>
-						</label>
-					</li>
-					<li>
-						<button type="submit">Add Color</button>
-					</li>
-				</ul>
-			</form>
-		</Show>
+								/>
+							</label>
+						</li>
+						<li class="new_swatch_rgb">
+							<label>
+								"R: "
+								<input
+									type="number"
+									required
+									min="0"
+									max="255"
+									prop:value=r
+									on:input=move |ev| {
+										set_r.set(event_target_value(&ev));
+									}
+								/>
+							</label>
+						</li>
+						<li class="new_swatch_rgb">
+							<label>
+								"G: "
+								<input
+									type="number"
+									required
+									min="0"
+									max="255"
+									prop:value=g
+									on:input=move |ev| {
+										set_g.set(event_target_value(&ev));
+									}
+								/>
+							</label>
+						</li>
+						<li class="new_swatch_rgb">
+							<label>
+								"B: "
+								<input
+									type="number"
+									required
+									min="0"
+									max="255"
+									prop:value=b
+									on:input=move |ev| {
+										set_b.set(event_target_value(&ev));
+									}
+								/>
+							</label>
+						</li>
+						<li>
+							<label>
+								"Color: "
+								<input
+									type="color"
+									prop:value=color
+									on:input=move |ev| {
+										let value = event_target_value(&ev);
+										if let Some(rgb) = Rgb::from_hex(&value) {
+											set_r.set(rgb.red.to_string());
+											set_g.set(rgb.green.to_string());
+											set_b.set(rgb.blue.to_string());
+										}
+										set_color.set(value);
+									}
+								/>
+							</label>
+						</li>
+						<li>
+							<button type="submit">Add Color</button>
+						</li>
+					</ul>
+				</form>
+			</Show>
+		</div>
 	}
 }
